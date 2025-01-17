d956c58ee41f9a7c8f89ae9ef8efe2c4f86942e7
JonRoss767 <jhross767@gmail.com>
2025-01-17 10:24:33 -0500

    Updated NavBar
    -Updated CSS
    -Transitioned to mapping routed to condense code to be more flexible

--- app/componets/NavBar.tsx
+++ app/componets/NavBar.tsx
@@ -1,43 +1,43 @@
 import Link from "next/link";
 import "../globals.css";
+//import "../css/NavBar.css";
 
 export default function NavBar() {
+  const navItems = [
+    { name: "Profile", path: "/user" },
+    { name: "Tracks", path: "/tracks" },
+    { name: "Albums", path: "/album" },
+    { name: "Artists", path: "/artists" },
+    { name: "About", path: "/about" },
+  ];
+
   return (
-    <nav className="bg-green-500 h-screen">
-      <div className="text-white text-center text-4xl">
-        <div className="flex flex-col items-center p-4 space-y-4">
-          <Link
-            href="/user"
-            className="border-4 w-52 border-black bg-gray-800 text-white p-4 mt-4 mb-4 font-bold"
-          >
-            Profile
-          </Link>
-          <Link
-            href="/tracks"
-            className="border-4 w-52 border-black bg-gray-800 text-white p-4 mt-4 mb-4 font-bold"
-          >
-            Tracks
-          </Link>
-          <Link
-            href="/album"
-            className="border-4 w-52 border-black bg-gray-800 text-white p-4 mt-4 mb-4 font-bold"
-          >
-            Albums
-          </Link>
-          <Link
-            href="/artists"
-            className="border-4 w-52 border-black bg-gray-800 text-white p-4 mt-4 mb-4 font-bold"
-          >
-            Artists
-          </Link>
-          <Link
-            href="/about"
-            className="border-4 w-52 border-black bg-gray-800 text-white p-4 mt-4 mb-4 font-bold"
-          >
-            About
-          </Link>
-        </div>
-      </div>
+    <nav className="bg-green-500 h-full flex flex-col items-center">
+      <ul className=" w-[85%] mt-5 flex flex-col gap-3">
+        {navItems.map((item) => (
+          <li key={item.path} className="">
+            <Link
+              className={`
+              border-2 
+              border-gray-800
+              bg-gray-700
+              p-4
+              w-full
+              text-white
+              text-3xl
+              font-normal
+              hover:text-green-500
+              transition-all
+              block
+              text-center
+              `}
+              href={item.path}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
